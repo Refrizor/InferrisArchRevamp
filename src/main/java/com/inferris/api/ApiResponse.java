15a993ddca0ddda60366080091ef89ab58d44b63
Refrizor <retizao.tv@gmail.com>
2024-09-02 22:02:31 -0400

    Add ApiResponse

--- src/main/java/com/inferris/api/ApiResponse.java
+++ src/main/java/com/inferris/api/ApiResponse.java
@@ -1,16 +1,6 @@
 package com.inferris.api;
 
-public class ApiResponse<T> {
-    private boolean success;
-    private T player;
-
-    public T getPlayer() {
-        return player;
-    }
-
-    public boolean isSuccess() {
-        return success;
-    }
+public record ApiResponse<T>(boolean success, T player) {
 
     // Getters and setters...
 }

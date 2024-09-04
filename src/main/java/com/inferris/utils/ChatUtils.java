21b256dcb56dede8d8d73b628322284a6b23115c
Refrizor <retizao.tv@gmail.com>
2024-09-04 00:10:36 -0400

    Remove unused method

--- src/main/java/com/inferris/utils/ChatUtils.java
+++ src/main/java/com/inferris/utils/ChatUtils.java
@@ -1,33 +1,8 @@
 package com.inferris.utils;
 
-import net.md_5.bungee.api.ChatColor;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class ChatUtils {
-    public static String translateToHex(String message) {
-        final Pattern pattern = Pattern.compile("(?<!\\\\)(#[a-fA-F0-9]{6})");
-        Matcher matcher = pattern.matcher(message);
-        while (matcher.find()) {
-            String hexCode = message.substring(matcher.start(), matcher.end());
-            String replaceSharp = hexCode.replace('#', 'x');
-
-            char[] ch = replaceSharp.toCharArray();
-            StringBuilder builder = new StringBuilder();
-            for (char c : ch) {
-                builder.append("&").append(c);
-            }
-
-            message = message.replace(hexCode, builder.toString());
-            matcher = pattern.matcher(message);
-        }
-        return ChatColor.translateAlternateColorCodes('&', message);
-    }
 
     public static String translateAlternateColorCodes(char altColorChar, String textToTranslate) {
         return textToTranslate.replace(altColorChar, '§');
-
     }
-
 }

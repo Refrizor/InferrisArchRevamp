17829eb09be6b563a79bbbd8501ffa327909d0ea
Refrizor <retizao.tv@gmail.com>
2024-09-03 23:51:42 -0400

    Remove and refactor classes that weren't a part of the PD architecture change

--- src/main/java/com/inferris/Inferris.java
+++ src/main/java/com/inferris/Inferris.java
@@ -4,9 +4,6 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.inferris.api.PlayerDataApiClient;
 import com.inferris.commands.CommandAPI;
-import com.inferris.config.ConfigType;
-import com.inferris.config.ConfigurationHandler;
-import com.inferris.events.EventJoin;
 import com.inferris.cache.PlayerDataCache;
 import com.inferris.module.PlayerDataModule;
 import com.inferris.service.PlayerDataService;
@@ -15,14 +12,11 @@ import net.md_5.bungee.api.plugin.PluginManager;
 
 public class Inferris extends Plugin {
     private static Inferris instance;
-    private final ConfigurationHandler configurationHandler = ConfigurationHandler.getInstance();
 
     @Override
     public void onEnable() {
         instance = this;
-        getLogger().info("Inferris module is enabled!");
-        configurationHandler.loadProperties(ConfigType.PROPERTIES);
-        configurationHandler.loadProperties(ConfigType.DATABASE);
+        getLogger().info("Inferris plugin is enabled!");
 
         Injector injector = Guice.createInjector(new PlayerDataModule());
         PlayerDataService playerDataService = injector.getInstance(PlayerDataService.class);
@@ -30,7 +24,6 @@ public class Inferris extends Plugin {
         PlayerDataCache playerDataCache = injector.getInstance(PlayerDataCache.class);
 
         PluginManager pluginManager = this.getProxy().getPluginManager();
-        pluginManager.registerListener(this, new EventJoin(playerDataService));
         pluginManager.registerCommand(this, new CommandAPI("api", playerDataService, playerDataCache));
     }
 
